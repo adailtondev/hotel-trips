3684b2317a08412a469abc2e8a23043d76d0a3f9
adailtondev <adailtonh.dev@gmail.com>
2023-07-29 00:22:41 -0300

    auth implantation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
 
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const {data} = useSession()
+
+  
+
   return (
-    <h1>HOME-PAGE</h1>
-  )
+    <div className='bg-primaryDarker'>
+      <h1 className='text-4xl'></h1>
+    </div>
+  );
 }
