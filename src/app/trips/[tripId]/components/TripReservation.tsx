c6d0737e5c25e881ca839aa12f277626d6075678
adailtondev <adailtonh.dev@gmail.com>
2023-08-01 11:33:11 -0300

    changes on trip page

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -12,28 +12,30 @@ interface TripReservationProps {
 
 const TripReservation = ({ trip }: TripReservationProps) => {
   return (
-    <div>
-      <div className="flex flex-col px-5">
-        <div className="flex gap-3">
-          <DataPicker
-            className="w-full"
-            placeholderText="Data de Início"
-            onChange={() => {}}
-          />
-          <DataPicker
-            className="w-full"
-            placeholderText="Data Final"
-            onChange={() => {}}
-          />
-        </div>
-        <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
+    <div className="flex flex-col px-5">
+      <div className="flex gap-3">
+        <DataPicker
+          className="w-full"
+          placeholderText="Data de Início"
+          onChange={() => {}}
+        />
+        <DataPicker
+          className="w-full"
+          placeholderText="Data Final"
+          onChange={() => {}}
+        />
+      </div>
+      <Input
+        placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+        className="mt-4"
+      />
 
-        <div className="flex justify-between mt-3">
-            <p className="font-medium text-sm text-primaryDarker">Total: </p>
-            <p className="font-medium text-sm text-primaryDarker">R$ 2.500,00</p>
-        </div>
-
-        <Button className="mt-3">Reservar agora</Button>
+      <div className="flex justify-between mt-3">
+        <p className="font-medium text-sm text-primaryDarker">Total: </p>
+        <p className="font-medium text-sm text-primaryDarker">R$2500</p>
+      </div>
+      <div className="pb-10 border-b border-grayLighter w-full">
+        <Button className="mt-3 w-full">Reservar agora</Button>
       </div>
     </div>
   );
