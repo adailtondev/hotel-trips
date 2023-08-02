70a29530f3bbfd195bb0e207c3b12c129fe5fb57
adailtondev <adailtonh.dev@gmail.com>
2023-08-02 14:54:51 -0300

    add auth to trip confirmation

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -8,10 +8,16 @@ import ReactCountryFlag from "react-country-flag";
 import { format } from "date-fns/esm";
 import ptBR from "date-fns/locale/pt-BR";
 import Button from "@/components/Button";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>();
   const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const router = useRouter()
+
+  const {status} = useSession()
 
   const searchParams = useSearchParams();
 
@@ -32,8 +38,12 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
       setTotalPrice(totalPrice);
     };
 
+    if (status === 'unauthenticated') {
+        router.push('/')
+    }
+
     fetchTrip();
-  }, []);
+  }, [status]);
 
   if (!trip) return null;
 
